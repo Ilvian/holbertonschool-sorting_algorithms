925ebc844af88831a62c3957a1e251b355434ea1
Ilvian <d.ilvian1993@gmail.com>
2023-09-12 13:34:13 -0700

    task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,7 +11,7 @@ void bubble_sort(int *array, size_t size)
 	int temp;
 
 	if (array == NULL || size == 0)
-                return;
+		return;
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - 1 - i; j++)
